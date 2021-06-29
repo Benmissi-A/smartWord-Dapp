75047aa7cad913eb399136e9df59fe317b5e1ee9
Benmissi-A <aminebenmissi@hotmail.fr>
2021-06-29 16:43:41 +0200

    push

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Dapp from "./Dapp";
 import { useContract } from "web3-hooks";
 import { counterAddress, counterAbi } from "./contracts/Counter";
+import SmartWord  from "./contracts/SmartWord";
 
-export const CounterContext = React.createContext(null);
+export const SmartWord = React.createContext(null);
 
 function App() {
-  const counter = useContract(counterAddress, counterAbi);
+  const smartWord = useContract(counterAddress, counterAbi);
   return (
-    <CounterContext.Provider value={counter}>
+    <CounterContext.Provider value={smartWord}>
       <Dapp />
     </CounterContext.Provider>
   );
