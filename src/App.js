d2e09714eda983c0ad00320db48de310b8cbd29c
Benmissi-A <aminebenmissi@hotmail.fr>
2021-07-01 16:29:38 +0200

    push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Dapp from "./Dapp";
 import { useContract } from "web3-hooks";
 import { smartWordAddress, smartWordAbi } from "./contracts/SmartWord";
+import { Box } from "@chakra-ui/react";
 
 export const SmartWordContext = React.createContext(null);
 
@@ -9,7 +10,9 @@ function App() {
   const smartWord = useContract(smartWordAddress, smartWordAbi);
   return (
     <SmartWordContext.Provider value={smartWord}>
-      <Dapp />
+      <Box minHeight="100vh" >
+        <Dapp />
+      </Box>
     </SmartWordContext.Provider>
   );
 }
