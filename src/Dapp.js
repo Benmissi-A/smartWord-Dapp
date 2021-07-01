733565146713a6a28db23399ad3e43dadfd2c026
Benmissi-A <aminebenmissi@hotmail.fr>
2021-07-01 15:15:21 +0200

    push

--- src/Dapp.js
+++ src/Dapp.js
@@ -5,12 +5,26 @@ import { Container, Box ,Heading} from "@chakra-ui/react";
 
 
 const Dapp = () => {
+    const [web3State, login] = useContext(Web3Context);
+
   return (
     <Container>
       <Box>
         <Heading>Hello</Heading>
       </Box>
+      <p>MetaMask installed: {web3State.isMetaMask ? "yes" : "no"}</p>
+      <p>Web3: {web3State.isWeb3 ? "injected" : "no-injected"}</p>
+      <p>logged: {web3State.isLogged ? "yes" : "no"}</p>
+      {!web3State.isLogged && (
+        <>
+          <button onClick={login}>login</button>
+        </>
+      )}
+      <p>Network id: {web3State.chainId}</p>
+      <p>Network name: {web3State.networkName}</p>
+      <p>account: {web3State.account}</p>
+      <p>Balance: {web3State.balance}</p>
     </Container>
-  )
+  );
 }
 export default Dapp
